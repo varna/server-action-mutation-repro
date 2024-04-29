c1b46c30b5f1f333b4d03cca0f09f19e589f2036
Paulius Varna <paul.varna@gmail.com>
2024-04-29 12:25:42 +0300

    add revalidatePath

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function queryServerAction() {
 	console.log("logging: queryServerAction");
-	return "query server action response";
+	return `query server action response: ${new Date().toISOString()}`;
 }
 
 export async function mutationServerAction(input: string) {
 	console.log("logging: mutationServerAction", input);
+	revalidatePath("/");
 	return `mutation server action response: ${input}`;
 }
