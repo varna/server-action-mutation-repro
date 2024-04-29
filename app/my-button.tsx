45090d79e719bef2f3e39159828a69bd92f73430
Paulius Varna <paul.varna@gmail.com>
2024-04-29 12:30:51 +0300

    add searchParams

--- app/my-button.tsx
+++ app/my-button.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { useState } from "react";
 import { queryServerAction, mutationServerAction } from "./actions";
+import { parseAsString, useQueryState } from "nuqs";
 
 export default function MyButton() {
-	const [arg, setArg] = useState("input");
 	const [res, setRes] = useState("click to run mutation");
+	const [arg, setArg] = useQueryState(
+		"email",
+		parseAsString.withDefault("").withOptions({
+			shallow: false,
+		}),
+	);
 
 	return (
 		<>
